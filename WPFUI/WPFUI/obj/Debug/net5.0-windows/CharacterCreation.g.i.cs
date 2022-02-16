0db136f0cdf1973337b7b4d562f9cf3c16206b7e
Rezzick <jarin245@gmail.com>
2022-02-16 07:46:27 -0600

    Lesson 19.3 - Move data and image files out of Engine project

--- WPFUI/WPFUI/obj/Debug/net5.0-windows/CharacterCreation.g.i.cs
+++ WPFUI/WPFUI/obj/Debug/net5.0-windows/CharacterCreation.g.i.cs
@@ -54,7 +54,7 @@ namespace WPFUI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFUI;V1.0.0.0;component/charactercreation.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFUI;component/charactercreation.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\CharacterCreation.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- WPFUI/WPFUI/obj/Debug/net5.0-windows/TradeScreen.g.i.cs
+++ WPFUI/WPFUI/obj/Debug/net5.0-windows/TradeScreen.g.i.cs
@@ -53,7 +53,7 @@ namespace WPFUI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFUI;V1.0.0.0;component/tradescreen.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFUI;component/tradescreen.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\TradeScreen.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
